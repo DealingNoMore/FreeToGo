47c895a3c66ca8bf9d99cea3af9b00c864ce749e
zhanglinhao <1260788704@qq.com>
2024-05-06 11:28:09 +0800

    init repo

--- src/main/java/com/freetogo/freetogo/hellocontroller.java
+++ src/main/java/com/freetogo/freetogo/hellocontroller.java
@@ -1,2 +1,12 @@
-package com.freetogo.freetogo;public class hellocontroller {
+package com.freetogo.freetogo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class hellocontroller {
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello Spring Boot 222!";
+    }
 }
